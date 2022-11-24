54e3308bb3c6c2e04f93c28f300bf9cdd6c11759
Lukas Baule <lukas.baule@gmail.com>
2022-11-25 00:11:13 +0100

    complex testing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <SFML/Graphics.hpp>
-#include <math.h>
+#include <iostream>
+#include <complex>
+#include <cmath>
 
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1500,1000), "Fourier series", sf::Style::Default);
+    std::complex<double> c(10.0, 2.0);
+    std::cout << std::real(c) << std::endl;
+    std::cout << std::imag(c) << std::endl;
+    std::cout << std::pow(c, 2) << std::endl;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -13,11 +19,12 @@ int main() {
                 window.close();
         }
 
-        for (int x = 0; x <= 1500; x++) { 
-            for (int y = 0; y <= 1000; y++) {
+        for (int x = 0; x <= 1500; x+=2) { 
+            for (int y = 0; y <= 1000; y+=8) {
                 sf:: Vertex ver;
                 ver.position = sf::Vector2f(x,y);
                 ver.color = sf::Color::Red;
+
                 window.draw(&ver, 1, sf::Points);
             }
         }
